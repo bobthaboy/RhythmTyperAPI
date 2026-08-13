1ef442f10858ba7c5445ef69cfd270c8f6eeae33
Bob Thaboy <bobthaboy@gmail.com>
2026-08-13 12:30:10 -0700

    Add files via upload
    Updated for database migration

--- RhythmTyperAPI.py
+++ RhythmTyperAPI.py
@@ -907,15 +907,86 @@ class BestScores:
     count: int
 
 
+@dataclass
+class UserSearch:
+    user_id: str
+    username: str
+    total_pp: float
+    global_rank: int
+    country: str
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            user_id=data.get("userId"),
+            username=data.get("username"),
+            total_pp=data.get("totalPP"),
+            global_rank=data.get("globalRank"),
+            country=data.get("country")
+        )
+
+
 class RhythmTyperAPI:
-    def _fetch(self, url):
-        response = requests.get(url)
+    BASE_URL = "https://api.rhythmtyper.net"
+
+    def _fetch(self, path, **params):
+        response = requests.get(
+            f"{self.BASE_URL}/{path.lstrip('/')}",
+            params=params,
+        )
+        response.raise_for_status()
         return response.json()
+
+    def get_beatmap(self, mapset_id: str) -> BeatmapsExtended:
+        """
+        Returns given beatmap.
+        """
+        response = self._fetch(
+            "getBeatmaps",
+            limit=1,
+            mapsetId=mapset_id,
+        )
+
+        song_list = response.get("beatmaps", [])
+
+        if not song_list:
+            raise ValueError(f"No beatmap found for mapset ID {mapset_id}")
+
+        song = song_list[0]
+
+        for diff in song["difficulties"]:
+            if "noteCount" in diff:
+                diff["tapCount"] = diff["noteCount"]
+                diff["holdCount"] /= 2
+
+        song["difficulties"] = [
+            CustomDifficulty.from_dict(item)
+            for item in song["difficulties"]
+        ]
+
+        if "versionHistory" in song:
+            song["versionHistory"] = [
+                VersionHistory.from_dict(item)
+                for item in song["versionHistory"]
+            ]
+
+        return BeatmapsExtended.from_dict(song)
+
+    def user_lookup(self, username: str, limit: int = 50) -> list[UserSearch]:
+        """Returns the users that show up on the search."""
+        if len(username) < 2:
+            raise ValueError("Username must be at least 2 characters long.")
+
+        response = self._fetch(
+            "v2/users/search",
+            query=username,
+            limit=limit,
+        )
+        return [UserSearch.from_dict(user) for user in response]
 
     def get_user_profile(self, user_id: str) -> UserProfile:
         """Return a user's profile."""
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/v2/profile/{user_id}"
-        response = self._fetch(url)
+        response = self._fetch(f"v2/profile/{user_id}")
 
         response["createdAt"] = parse_timestamp(response.get("createdAt"))
         response["profilePictureUpdatedAt"] = parse_timestamp(response.get("profilePictureUpdatedAt"))
@@ -953,8 +1024,8 @@ class RhythmTyperAPI:
 
     def first_place_scores(self, user_id: str) -> FirstPlaceScores:
         """Returns all the first place scores of a user."""
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/v2/user/{user_id}/firstPlaceScores"
-        response = self._fetch(url)
+        response = self._fetch(f"v2/user/{user_id}/firstPlaceScores")
+
         response["scores"] = [
             Scores.from_dict(item)
             for item in response["scores"]
@@ -963,57 +1034,83 @@ class RhythmTyperAPI:
 
     def activity_heatmap(self, user_id: str) -> dict:
         """Returns a dictionary containing a date as the key and play count as the value."""
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/v2/stats/{user_id}/charts?type=heatmap"
-        response = self._fetch(url)
+        response = self._fetch(
+            f"v2/stats/{user_id}/charts",
+            type="heatmap"
+        )
+
         response = {datetime.strptime(date_str, "%Y-%m-%d").date(): value
                     for date_str, value in response.items()}
         return response
 
     def get_user_favorites(self, user_id: str) -> list[Favorites]:
         """Returns all maps a user favorited."""
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/getUserFavorites/{user_id}"
-        response = self._fetch(url)
+        response = self._fetch(
+            f"getUserFavorites/{user_id}",
+        )
+
         favorites_list = response.get("favorites", [])
         return [Favorites.from_dict(fav) for fav in favorites_list]
 
     def get_most_played_beatmaps(self, user_id: str, limit: int = 50) -> list[MostPlayed]:
         """Returns user's most played maps."""
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/getMostPlayedBeatmaps/{user_id}?limit={limit}"
-        response = self._fetch(url)
+        response = self._fetch(
+            f"getMostPlayedBeatmaps/{user_id}",
+            limit=limit
+        )
+
         most_played_list = response.get("mostPlayed", [])
         for song in most_played_list:
             song["lastPlayed"] = parse_timestamp(response.get("lastPlayed"))
         return [MostPlayed.from_dict(song) for song in most_played_list]
 
-    def get_beatmaps(self, mapper_id: str = None, limit: int = 50, cursor: str = None, status: str = "ranked",
+    def get_user_beatmaps(self, mapper_id: str, cursor: str = None, limit: int = 50) -> UserBeatmaps:
+        """
+        Returns beatmaps uploaded by a mapper_id.
+        """
+        response = self._fetch(
+            "getBeatmaps",
+            mapperId=mapper_id,
+            cursor=cursor,
+            limit=limit
+        )
+
+        song_list = response.get("beatmaps", [])
+        for song in song_list:
+            for diff in song["difficulties"]:
+                if "noteCount" in diff:
+                    diff["tapCount"] = diff["noteCount"]
+                    diff["holdCount"] /= 2
+
+            song["difficulties"] = [
+                CustomDifficulty.from_dict(item)
+                for item in song["difficulties"]
+            ]
+
+            if "versionHistory" in song:
+                song["versionHistory"] = [
+                    VersionHistory.from_dict(item)
+                    for item in song["versionHistory"]
+                ]
+
+        return UserBeatmaps.from_dict(response)
+
+    def get_beatmaps(self, limit: int = 50, cursor: str = None, status: str = "ranked",
                      sort: str = None,
                      explicit: bool = True, language: str = "all") -> UserBeatmaps:
         """
-        Returns beatmaps. Accepts mapper_id. \n
+        Returns beatmaps. \n
         Valid statuses are: "all", "ranked", "loved", "qualified", "nominated", "unranked", "custom".
         """
-
-        if mapper_id:
-            mapper_id = "?mapperId=" + mapper_id
-        else:
-            mapper_id = ""
-        if cursor:
-            cursor = "&cursor=" + cursor
-        else:
-            cursor = ""
-        if status:
-            status = "&status=" + status
-        else:
-            status = ""
-        if sort:
-            sort = "&sortBy=" + sort
-        else:
-            sort = ""
-        explicit = f"&showExplicit={str(explicit).lower()}"
-        language = "&language=" + language
-
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/getBeatmaps{mapper_id}?limit={limit}{cursor}{status}{sort}{explicit}{language}"
-        response = self._fetch(url)
+        response = self._fetch(
+            "getBeatmaps",
+            limit=limit,
+            cursor=cursor,
+            status=status,
+            sortBy=sort,
+            showExplicit=str(explicit).lower(),
+            language=language,
+        )
 
         song_list = response.get("beatmaps", [])
         for song in song_list:
@@ -1037,8 +1134,8 @@ class RhythmTyperAPI:
 
     def get_nominations(self, user_id: str) -> list[NominatedBeatmaps]:
         """Returns user's nominations."""
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/getNominatedBeatmaps/{user_id}"
-        response = self._fetch(url)
+        response = self._fetch(f"getNominatedBeatmaps/{user_id}")
+
         song_list = response.get("beatmaps", [])
         for song in song_list:
             song["difficulties"] = [
@@ -1059,19 +1156,22 @@ class RhythmTyperAPI:
         Returns global leaderboards. Accepts country. \n
         Valid sorts are: "pp", "score".
         """
-        if country:
-            country = "&country=" + country
-        else:
-            country = ""
-
         match sort:
             case "pp":
                 sort = "totalPP"
             case "score":
                 sort = "rankedScore"
+            case _:
+                raise ValueError('Sort must be "pp" or "score".')
 
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/v2/leaderboard?limit={limit}&offset={offset}{country}&sortBy={sort}"
-        response = self._fetch(url)
+        response = self._fetch(
+            "v2/leaderboard",
+            limit=limit,
+            offset=offset,
+            country=country,
+            sortBy=sort
+        )
+
         return [GlobalLeaderboard.from_dict(item) for item in response]
 
     def leaderboard_countries(self, limit: int = 50, offset: int = 0, sort: str = "pp") -> list[CountryLeaderboard]:
@@ -1082,56 +1182,68 @@ class RhythmTyperAPI:
             case "score":
                 sort = "rankedScore"
 
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/v2/leaderboard/countries?limit={limit}&offset={offset}&sortBy={sort}"
-        response = self._fetch(url)
+        response = self._fetch(
+            "v2/leaderboard/countries",
+            limit=limit,
+            offset=offset,
+            sortBy=sort
+        )
+
         return [CountryLeaderboard.from_dict(item) for item in response["countries"]]
 
     def leaderboard_top_plays(self, limit: int = 50, offset: int = 0, unique: bool = None) -> list[TopPlaysLeaderboard]:
         """Returns the top PP scores leaderboard."""
-        if unique is not None:
-            unique = f"&unique={str(unique).lower()}"
-        else:
-            unique = ""
+        response = self._fetch(
+            "v2/leaderboard/plays",
+            limit=limit,
+            offset=offset,
+            unique=str(unique).lower() if unique is not None else None
+        )
 
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/v2/leaderboard/plays?limit={limit}&offset={offset}{unique}"
-        response = self._fetch(url)
         return [TopPlaysLeaderboard.from_dict(item) for item in response["plays"]]
 
     def get_difficulties(self, beatmap_id: str) -> list[Difficulty]:
         """Returns the difficulties for a map."""
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/v2/beatmap/{beatmap_id}/difficulties"
-        response = self._fetch(url)
+        response = self._fetch(f"v2/beatmap/{beatmap_id}/difficulties")
+
         return [Difficulty.from_dict(item) for item in response]
 
     def beatmap_leaderboard(self, beatmap_id: str, difficulty: str, limit: int = 50) -> list[BeatmapLeaderboard]:
         """Returns the leaderboards for a beatmap."""
-        difficulty = difficulty.replace("+", "%2B")
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/v2/beatmap/{beatmap_id}/leaderboard?difficulty={difficulty}&limit={limit}"
-        response = self._fetch(url)
+        response = self._fetch(
+            f"v2/beatmap/{beatmap_id}/leaderboard",
+            difficulty=difficulty,
+            limit=limit
+        )
+
         return [BeatmapLeaderboard.from_dict(item) for item in response]
 
     def beatmap_comments(self, beatmap_id: str, limit: int = 50) -> list[Comments]:
         """Returns the comments on a mapset."""
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/getComments/{beatmap_id}?limit={limit}"
-        response = self._fetch(url)
+        response = self._fetch(
+            f"getComments/{beatmap_id}",
+            limit=limit
+        )
+
         comments = response["comments"]
         return [Comments.from_dict(item) for item in comments]
 
     def unplayed_difficulties(self, user_id: str, limit: int = 100) -> UnplayedDifficulties:
         """Returns all the difficulties a user hasn't played."""
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/v2/user/{user_id}/unplayedDifficulties?limit={limit}"
-        response = self._fetch(url)
+        response = self._fetch(
+            f"v2/user/{user_id}/unplayedDifficulties",
+            limit=limit
+        )
+
         response["unplayed"] = [Unplayed.from_dict(item) for item in response["unplayed"]]
         return UnplayedDifficulties(**response)
 
     def user_best_scores(self, user_id: str, limit: int = 100) -> BestScores:
         """Returns the user's best scores by highest Ranked Score."""
-        url = f"https://us-central1-rhythm-typer.cloudfunctions.net/api/v2/user/{user_id}/bestScores?limit={limit}"
-        response = self._fetch(url)
+        response = self._fetch(
+            f"v2/user/{user_id}/bestScores",
+            limit=limit
+        )
+
         response["scores"] = [BestScore.from_dict(item) for item in response["scores"]]
         return BestScores(**response)
-
-# # CODE GOES HERE
-
-# for attr, value in vars(me[0]).items():
-#     print(f"{attr}: {value}")
